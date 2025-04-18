42677798e1dbcfb60c7799681211e2c4227db1d0
Zulfikar42 <158750348+Zulfikar42@users.noreply.github.com>
2025-04-18 15:50:12 +0530

    Update fetch_from_dropbox.py

--- fetch_from_dropbox.py
+++ fetch_from_dropbox.py
@@ -1,1 +1,21 @@
+import dropbox
+
+# Dropbox access token (get it from Dropbox App console)
+DROPBOX_ACCESS_TOKEN = 'your_dropbox_access_token'
+
+def fetch_image_from_dropbox():
+    dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
+    
+    # Specify the image file path in Dropbox
+    image_path = '/path_to_your_image_in_dropbox/suspect.jpg'
+    
+    # Download the file
+    _, response = dbx.files_download(image_path)
+    
+    # Save the file locally
+    local_path = '/tmp/suspect.jpg'  # Temporary path
+    with open(local_path, 'wb') as f:
+        f.write(response.content)
+    
+    return local_path
 
